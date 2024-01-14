92516d76ce0bdd88e61be722f26a4dfd1d3620a3
Dmitry <101152412+Sauberr@users.noreply.github.com>
2024-01-14 19:09:17 +0300

    Delete venv directory

--- venv/Lib/site-packages/easy_thumbnails/VIL/__init__.py
+++
@@ -1,9 +0,0 @@
-def is_available() -> bool:
-    """
-    Returns True if SVG support should be available.
-    """
-    try:
-        import easy_thumbnails.VIL.Image
-        return True
-    except ImportError:
-        return False
